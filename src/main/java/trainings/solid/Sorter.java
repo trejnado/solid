a17abf2f9bc80a6281e4d9255f173483c4a38731
Przemysław Wojnowski <esperanto@cumego.com>
2016-01-21 14:50:29 +0100

    OCP - Move selection of sorting strategy to a map
    This makes sorting selection method closed for adding new strategies,
    but open for extension by adding new mappings.

--- src/main/java/trainings/solid/Sorter.java
+++ src/main/java/trainings/solid/Sorter.java
@@ -4,11 +4,17 @@ import trainings.solid.comparators.AssistsComparator;
 import trainings.solid.comparators.NameComparator;
 import trainings.solid.comparators.PointsComparator;
 
-import java.util.List;
-
-import java.util.Collections;
+import java.util.*;
 
 public class Sorter implements DataProcessor {
+
+    private Map<String, Comparator<Player>> fields = new HashMap<>();
+
+    {
+        fields.put("name", new NameComparator());
+        fields.put("points", new PointsComparator());
+        fields.put("assist", new AssistsComparator());
+    }
 
     private String sortBy;
 
@@ -19,18 +25,10 @@ public class Sorter implements DataProcessor {
     @Override
     public void process(List<Player> players) {
         System.out.println("Sorting by: " + sortBy);
-        switch (sortBy) {
-            case "name":
-                Collections.sort(players, new NameComparator() );
-                break;
-            case "points":
-                Collections.sort(players, new PointsComparator() );
-                break;
-            case "assist":
-                Collections.sort(players, new AssistsComparator());
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown option: " + sortBy);
+        Comparator<Player> comparator = fields.get(sortBy);
+        if (comparator == null) {
+            throw new IllegalArgumentException("Unknown option: " + sortBy);
         }
+        Collections.sort(players, comparator);
     }
 }
